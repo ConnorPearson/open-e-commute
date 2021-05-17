cc3dc3dd2551ae981c4b2d0a86bcd04371dc6925
phoenix <ca21vw@protonmail.com>
2021-05-17 23:49:27 +0100

    Initial commit

---
+++ app/src/main/java/com/example/open_e_commute/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.open_e_commute;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
