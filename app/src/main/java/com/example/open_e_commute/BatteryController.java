7fbfed31eb723fb3ab68467101c47d9b014495b2
phoenix <ca21vw@protonmail.com>
2021-06-18 02:00:18 +0100

    style : Added base layout and assets for main-map-view.xml

---
+++ app/src/main/java/com/example/open_e_commute/BatteryController.java
@@ -0,0 +1,13 @@
+package com.example.open_e_commute;
+
+import java.io.File;
+
+public class BatteryController {
+    int maxCapacity;
+    int currentCapacity;
+
+    File[] whiteBattery;
+    File[] blackBattery;
+
+
+}
